0c8c2808b0b7c25603caee39ba4f6f674722f7a3
Borisz <borisz.voros@cheppers.com>
2016-04-18 08:28:56 +0200

    order feature added

---
+++ sites/all/modules/custom/matrix/templates/matrix_cart_print.tpl.php
@@ -0,0 +1,7 @@
+<div id="summary">
+  <div class="cart-row" data-row="row-x" data-product="<?php print $pid; ?>">
+    <div class="amount cell"><?php print $amount; ?></div>
+    <div class="content cell"><?php print $product; ?></div>
+    <div style="clear:both"></div>
+  </div>
+</div>
