06d692f0940ece012ec70a53c46f7ebec991b6cc
Borisz <borisz.voros@cheppers.com>
2016-04-20 16:50:56 +0200

    buttons fix, list layout fix

--- sites/all/modules/custom/matrix/templates/matrix_cart.tpl.php
+++ sites/all/modules/custom/matrix/templates/matrix_cart.tpl.php
@@ -1,8 +1,8 @@
 <div id="summary">
   <div class="cart-row" data-row="row-x" data-product="<?php print $pid; ?>">
-    <div class="cell no-width">
-      <input type="button" value="x" class="cart-delete">
-    </div>
+
+    <div class="content cell"><?php print $product; ?></div>
+
     <div class="cell no-width">
       <input type="button" value="-" class="cart-minus">
     </div>
@@ -10,7 +10,16 @@
     <div class="cell no-width">
       <input type="button" value="+" class="cart-plus">
     </div>
-    <div class="content cell"><?php print $product; ?></div>
+    <div class="markup cell">db</div>
+    <div class="weight cell"><?php print $weight; ?></div>
+    <div class="markup cell">kg</div>
+
+    <div class="weight cell"><?php print $price; ?></div>
+    <div class="markup cell">Ft</div>
+
+    <div class="cell no-width">
+      <input type="button" value="x" class="cart-delete">
+    </div>
     <div style="clear:both"></div>
   </div>
 </div>
