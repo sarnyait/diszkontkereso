d1528bd160590c44efd12c9b4116898fd43c6cc5
Borisz <borisz.voros@cheppers.com>
2016-04-20 17:28:27 +0200

    list layout fields added

--- sites/all/modules/custom/matrix/templates/matrix_cart_print.tpl.php
+++ sites/all/modules/custom/matrix/templates/matrix_cart_print.tpl.php
@@ -1,7 +1,19 @@
 <div id="summary">
   <div class="cart-row" data-row="row-x" data-product="<?php print $pid; ?>">
-    <div class="amount cell"><?php print $amount; ?></div>
+
     <div class="content cell"><?php print $product; ?></div>
+
+
+    <div class="amount cell"><?php print $amount; ?></div>
+
+    <div class="markup cell">db</div>
+    <div class="weight cell"><?php print $weight; ?></div>
+    <div class="markup cell">kg</div>
+
+    <div class="weight cell"><?php print $price; ?></div>
+    <div class="markup cell">Ft</div>
+
+
     <div style="clear:both"></div>
   </div>
 </div>
