715c29070af252c5d88b3418e0defd09cab64782
Borisz <borisz.voros@cheppers.com>
2016-05-07 17:12:08 +0200

    add to cart validate changed if there are more than two cart on the list we dont fire popups

--- sites/all/modules/custom/matrix/js/matrix_compare_page.js
+++ sites/all/modules/custom/matrix/js/matrix_compare_page.js
@@ -5,65 +5,23 @@
 
 
       var modal = $('[data-remodal-id=modal]').remodal();
-
-      function pushContent(s, shop) {
-        s = s.substr(s, s.length-1);
-        elements = s.split('&');
-        content = '';
-        c = parseInt(Math.random() * 10000000);
-        elements.forEach(function(e) {
-          parts = e.split('|');
-          test = checkCart(parts[0], parts[1]);
-          if (!test) {
-            $('#summary .cart-row.invisible')
-              .clone()
-              .removeClass('invisible')
-              .appendTo('#summary')
-              .attr('data-row', 'row-' + c)
-              .addClass(shop)
-            $('.cart-row[data-row="row-' + c + '"] .amount').html(parts[1]);
-            $('.cart-row[data-row="row-' + c + '"] .content').html(parts[0]);
-          }
-          c = parseInt(Math.random() * 10000000);
-        });
-        $('#summary').append(content);
-        $('#header .cell').each(function() {
-            $('#summary .cart-row.' + $(this).attr('data-name')).each(function() {
-              $('#summary').append($(this));
-            })
-          }
-        )
-
-      }
-
-      function checkCart(content, amount) {
-        var found = false;
-        $('#summary .cart-row').each(function() {
-          if ($('.content', this).html() == content) {
-            $('.amount', this).html(parseInt($('.amount', this).html())  + parseInt(amount));
-            found = true;
-          }
-        });
-        return found;
-      }
+      var modal2 = $('[data-remodal-id=modalSameShop]').remodal();
 
       rows = $('.starter_cell').length;
 
-
-      $('.matrix_val').val('1');
-
-
+      /*
+      * Set the amount of product will be added to the cart.
+      * */
       if ($('.matrix_val_change').length) {
         $('.matrix_val_change').on("click", function () {
-          source = '#' + $(this).attr('data-source');
-          w = $(source).next().attr('data-weight');
-          pid = $(this).attr('data-name');
+          source = '#' + $(this).data('source');
+          w = $(source).next().data('weight');
+          pid = $(this).data('name');
           if ($(this).attr('value') == '-') {
             if ($(source).val() > 0) {
               v = $(source).val() * 1 - 1;
               $(source).val(v);
               if (v == 0) {
-                //$(this).parent().parent().parent().addClass('faded');
                 $('.check[data-name="' + pid + '"]').trigger('click');
               }
             }
@@ -71,32 +29,43 @@
           else {
             v = $(source).val() * 1 + 1;
             $(source).val(v);
-            //$(this).parent().parent().parent().removeClass('faded');
-            //$(source).next().val(v * w); // weight
-
           }
           updateSumValues();
-
         })
       }
 
+      /*
+      * Push the selected list from a shop to the list.
+      * */
+
       if ($('.matrix_cart_button').length) {
         $('.matrix_cart_button').on("click", function () {
-          console.log($.cookie('diszkont'));
           var top = $(this).attr('data-name');
           var shopName = $('#header .cell[data-name="' + top + '"]').html();
           toSend = $.cookie('diszkont');
           if (toSend !== null) {
-            $('#cart-popup').attr('data-name', top);
+            // Popup if a cart already added before (avoid data loss)
+            $('.remodal').attr('data-name', top);
             $.post('/get-shop-name',
               {
                 order: $.cookie('diszkont')
               },
               function (data) {
-                $('.modal-message').html('Már van egy kosara (' + data + ')  a bevásárlólistáján. Mit tenne?');
-                $('.remodal-none').html('Marad a meglévő (' + data + ')');
-                $('.remodal-cancel').html('Kicserélem a most választottra (' + shopName + ')');
-                modal.open();
+                if (data == 'no-check') {
+                  toSend = $.cookie('diszkont');
+                  addToCart(top, toSend);
+                  window.location.href = 'matrix_cart';
+                }
+                else if (data != shopName) {
+                  $('.modal-message .modalShopName').html(data);
+                  $('.remodal-none .modalShopName').html(data);
+                  $('.remodal-cancel .modalNewShopName').html(shopName);
+                  modal.open();
+                }
+                else {
+                  $('.modal-message .modalShopName').html(data);
+                  modal2.open();
+                }
               });
           }
           else {
@@ -107,7 +76,7 @@
         });
       }
 
-      if ($('.remodal-none').length) {
+      if ($('.remodal-confirm').length) {
         $('.remodal-none').on("click", function () {
           //window.location.href = 'matrix_cart';
         });
@@ -144,7 +113,6 @@
             }
           }
         });
-        pushContent(content, top);
         $.cookie('diszkont', toSend, {path: '/'});
       }
 
@@ -171,17 +139,6 @@
 
         })
       }
-
-      /*if ($('.deleteRow').length) {
-        $('.deleteRow').on("click", function () {
-          if ($('.table-row').length > 1) {
-            name = $(this).attr('data-name');
-            humanName = $('.starter_cell[data-name="' + name + '"] label').html();
-            $(this).parent().remove();
-            $('select[name="addRow"]').append('<option value="' + name + '">' + humanName + '</option>');
-          }
-        })
-      }*/
 
       function cartModify(method, id, sid) {
         cart = $.cookie('diszkont');
@@ -225,7 +182,6 @@
           })
           reCart = reCart.substring(1);
           $.cookie('diszkont', reCart, {path: '/'});
-          console.log('SHOP LENGTH' + $('.item-from-' + sid).length);
           if (sid > 0 && $('.item-from-' + sid).length == 0) {
             $('.shop-header-' + sid).hide();
             $('.shop-block-' + sid).hide();
@@ -233,21 +189,13 @@
           if ($.cookie('diszkont') == '') {
             $.cookie('diszkont', null, {path: '/'});
             $('.shop-block').hide();
-            console.log('cart-deleted');
-
           }
-          /*if ($('.item').length == 1) {
-            $.cookie('diszkont', null, {path: '/'});
-            console.log('cart-deleted');
-          }*/
         }
-
       }
 
       if ($('.cart-delete').length) {
         $(document).on("click", ".cart-delete", function () {
           sid = $(this).attr('data-shop');
-          //$(this).parent().parent().parent().remove();
           $(this).closest('.item').remove();
           cartModify('remove', $(this).closest('.cart-row').attr('data-product'), sid);
           updateShopSums(sid);
@@ -257,20 +205,16 @@
           pid = $(this).attr('data-product');
           sid = $(this).attr('data-shop');
           cartModify('plus', pid, sid);
-
           updateProductSums(pid, 1);
           updateShopSums(sid);
-
         })
 
         $(document).on("click", ".cart-minus", function () {
           pid = $(this).attr('data-product');
           sid = $(this).attr('data-shop');
           cartModify('minus', pid, sid);
-
           updateProductSums(pid, -1);
           updateShopSums(sid);
-
         })
       }
 
@@ -280,44 +224,40 @@
           $('.item-from-' + shop).each(function () {
             pid = $(this).attr('data-pid');
             cartModify('remove', pid, shop);
-          })
+          });
           $('.shop-block-' + shop).hide();
-          console.log($.cookie('diszkont'));
-          console.log('NEED TO DELETE' + $.cookie('diszkont').length);
           if ($.cookie('diszkont').length == 0) {
             $.cookie('diszkont', null);
-            console.log('DELETED');
           }
-          ;
         })
       }
 
 
       function updateProductSums(pid, method) {
-        weight = $('.amount[data-product="'+pid+'"]').attr('data-weight') * 1;
-        price = $('.amount[data-product="'+pid+'"]').attr('data-price') * 1;
-        amount = $('.amount[data-product="'+pid+'"]').html() * 1;
+        weight = $('.amount[data-product="' + pid + '"]').data('weight') * 1;
+        price = $('.amount[data-product="' + pid + '"]').data('price') * 1;
+        amount = $('.amount[data-product="' + pid + '"]').html() * 1;
         if (method == 1 || amount > 0) {
           amount = amount + method;
         }
-        $('.amount[data-product="'+pid+'"]').html(amount);
-        $('.price[data-product="'+pid+'"]').html(amount * price);
-        $('.weight[data-product="'+pid+'"]').html(parseInt(amount * weight * 100) / 100);
+        $('.amount[data-product="' + pid + '"]').html(amount);
+        $('.price[data-product="' + pid + '"]').html(amount * price);
+        $('.weight[data-product="' + pid + '"]').html(parseInt(amount * weight * 100) / 100);
       }
 
       function updateShopSums(sid) {
         var weight = 0;
-        $('.weight[data-shop="'+sid+'"]').each(function() {
+        $('.weight[data-shop="' + sid + '"]').each(function() {
           weight += $(this).html() * 1;
         });
         weight = parseInt(weight * 10) / 10;
-        $('.cart-sum-weight[data-shop="'+sid+'"]').html(weight);
+        $('.cart-sum-weight[data-shop="' + sid + '"]').html(weight);
 
         var price = 0;
-        $('.price[data-shop="'+sid+'"]').each(function() {
+        $('.price[data-shop="' + sid + '"]').each(function() {
           price += $(this).html() * 1;
         });
-        $('.cart-sum-price[data-shop="'+sid+'"]').html(price);
+        $('.cart-sum-price[data-shop="' + sid + '"]').html(price);
 
       }
 
@@ -347,7 +287,8 @@
             order: $.cookie('diszkont')
           },
           function(data) {
-            window.location.href = 'print/' + data;
+            //window.location.href = 'print/' + data;
+            $.post('/print/' + data);
           })
       });
 
@@ -363,14 +304,10 @@
           var sumprice = 0;
           var sumweight = 0;
           var shop = $(this).attr('data-name');
-          console.log(shop);
           $('.cell[data-shop="' + shop + '"]').each(function () {
-            category = $(this).attr('data-category');
-            console.log('CAT' + category);
-            price = $(this).attr('data-price') * 1;
-            weight = $(this).attr('data-weight') * 1;
-            console.log('PRICE' + price);
-            console.log('WEIGHT' + weight);
+            category = $(this).data('category');
+            price = $(this).data('price') * 1;
+            weight = $(this).data('weight') * 1;
             if ($('.starter_cell.active[data-name="' + category + '"] input.matrix_val').length) {
               multiply = $('.starter_cell.active[data-name="' + category + '"] input.matrix_val').val();
               sumprice += price * multiply;
--- sites/all/modules/custom/matrix/templates/matrix_compare_page.tpl.php
+++ sites/all/modules/custom/matrix/templates/matrix_compare_page.tpl.php
@@ -1,10 +1,17 @@
 <div id="cart-popup" class="remodal" data-remodal-id="modal">
-  <div class="modal-message"></div>
+  <div class="modal-message"><?php print $message; ?></div>
   <a data-remodal-action="close" class="remodal-close"></a>
-  <a data-remodal-action="close" class="remodal-none" href="#">Marad</a>
-  <a data-remodal-action="cancel" class="remodal-cancel" href="#">Csere</a>
-  <a data-remodal-action="confirm" class="remodal-confirm" href="#">Kell mind</a>
+  <a data-remodal-action="close" class="remodal-none" href="#"><?php print $keep; ?></a>
+  <a data-remodal-action="cancel" class="remodal-cancel" href="#"><?php print $change; ?></a>
+  <a data-remodal-action="confirm" class="remodal-confirm" href="#"><?php print $keepboth; ?></a>
 </div>
+<div id="cart-popup-2" class="remodal" data-remodal-id="modalSameShop">
+  <div class="modal-message"><?php print $message2; ?></div>
+  <a data-remodal-action="close" class="remodal-close"></a>
+  <a data-remodal-action="cancel" class="remodal-none" href="#"><?php print $cancel; ?></a>
+  <a data-remodal-action="confirm" class="remodal-confirm" href="#"><?php print $add; ?></a>
+</div>
+
 <div class="compare-page">
 <?php
   print $table;
