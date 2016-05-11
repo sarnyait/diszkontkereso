699a30fdf8e7409c4a28ffc706a0ef3d1800c18c
Borisz <borisz.voros@cheppers.com>
2016-05-11 19:05:07 +0200

    weight selector in block only if weight related product

--- sites/all/modules/custom/matrix/templates/matrix_cart.tpl.php
+++ sites/all/modules/custom/matrix/templates/matrix_cart.tpl.php
@@ -12,6 +12,7 @@
         <input type="button" value="+" class="cart-plus" data-product="<?php print $pid; ?>" data-shop="<?php print $shop; ?>">
       </div>
     </div>
+    <?php if ($selector) { ?>
     <div class="amount-change">
       <div class="amount piece">
         <!--
@@ -20,15 +21,14 @@
         <div class="markup cell piece-unit" data-product="<?php print $pid; ?>">db</div>
         <div class="markup cell weight-unit element-remove" data-product="<?php print $pid; ?>">kg</div>
       </div>
-      <?php if ($weight > 0) { ?>
       <div class="markup cell piece-selector"><span class="label">change</span>
         <div class="amount-selector-popup element-invisible">
           <div class="amount-in-weight" data-product="<?php print $pid; ?>"><?php print t('Weight (kg)'); ?></div>
           <div class="amount-in-piece" data-product="<?php print $pid; ?>"><?php print t('Piece (pc)'); ?></div>
         </div>
       </div>
-      <?php } ?>
     </div>
+    <?php } ?>
 
 
     <div class="price cell" data-product="<?php print $pid; ?>" data-shop="<?php print $shop; ?>"><?php print $price; ?></div>
--- sites/all/modules/custom/matrix/templates/matrix_cart_block.tpl.php
+++ sites/all/modules/custom/matrix/templates/matrix_cart_block.tpl.php
@@ -11,20 +11,20 @@
         <input type="button" value="+" class="cart-plus" data-product="<?php print $pid; ?>" data-shop="<?php print $shop; ?>">
       </div>
     </div>
+    <?php if ($selector) { ?>
     <div class="amount-change">
       <div class="amount piece">
         <div class="markup cell piece-unit" data-product="<?php print $pid; ?>">db</div>
         <div class="markup cell weight-unit element-remove" data-product="<?php print $pid; ?>">kg</div>
       </div>
-      <?php if ($weight > 0) { ?>
         <div class="markup cell piece-selector"><span class="label">change</span>
           <div class="amount-selector-popup element-invisible">
             <div class="amount-in-weight" data-product="<?php print $pid; ?>"><?php print t('Weight (kg)'); ?></div>
             <div class="amount-in-piece" data-product="<?php print $pid; ?>"><?php print t('Piece (pc)'); ?></div>
           </div>
         </div>
-      <?php } ?>
     </div>
+    <?php } ?>
     <div class="cell no-width">
       <input type="button" value="x" class="cart-delete" data-shop="<?php print $shop; ?>">
     </div>
